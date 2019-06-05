4db31111a4cc0df4f7d7b5a874e31bb269854655
dKaiona <14drob35@gmail.com>
2019-06-05 12:16:54 -0600

    EndToEnd

--- code-or-explode/cypress/integration/danE2E.spec.js
+++ code-or-explode/cypress/integration/danE2E.spec.js
@@ -1,11 +1,24 @@
 
 
-describe('App starts', () => {
+
+describe('App goes to desk page and then goes to info-tech-page', () => {
     it('loads the desk page', () => {
         cy.visit('http://localhost:3000/#/desk')   
     }) 
-    it('clicks the link "bomb manual"', function() {
+    it('clicks the div "Wires"', function() {
         cy.visit('http://localhost:3000/#/info-tech-page')
         cy.contains('Wires').click()
+    })
+    it('clicks the div "Keypad"', function() {
+        cy.visit('http://localhost:3000/#/info-tech-page')
+        cy.contains('Keypad').click()
+    })
+    it('clicks the div "Maze"', function() {
+        cy.visit('http://localhost:3000/#/info-tech-page')
+        cy.contains('Maze').click()
+    })
+    it('clicks the div "Memory"', function() {
+        cy.visit('http://localhost:3000/#/info-tech-page')
+        cy.contains('Memory').click()
     })
 })
